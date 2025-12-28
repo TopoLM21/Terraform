7ba79d4ff76f79212c8519f2343cefadcd696d00
TopoLM21 <topoln21@gmail.com>
2025-12-29 00:34:01 +0300

    Add per-point surface state for simulation

---
+++ include/surface_point_state.h
@@ -0,0 +1,23 @@
+#pragma once
+
+class SurfacePointState {
+public:
+    SurfacePointState() = default;
+    SurfacePointState(double initialTemperatureKelvin,
+                      double albedo,
+                      double heatCapacity,
+                      double greenhouseOpacity,
+                      double minTemperatureKelvin);
+
+    double temperatureKelvin() const;
+    void setTemperatureKelvin(double temperatureKelvin);
+
+    void updateTemperature(double solarIrradiance, double dtSeconds);
+
+private:
+    double temperatureKelvin_ = 3.0;
+    double albedo_ = 0.0;
+    double heatCapacity_ = 1.0;
+    double greenhouseOpacity_ = 0.0;
+    double minTemperatureKelvin_ = 3.0;
+};
