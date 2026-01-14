e717d591e0ce428c47ed9e3ef2efba005936d328
TopoLM21 <topoln21@gmail.com>
2026-01-14 17:23:53 +0300

    Add star system top view tab

---
+++ include/StarSystemTopView.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <QtCore/QPointF>
+#include <QtCore/QVector>
+#include <QtCore/QString>
+#include <QtWidgets/QWidget>
+
+class StarSystemTopView : public QWidget {
+    Q_OBJECT
+
+public:
+    struct PlanetOrbit {
+        QString name;
+        double semiMajorAxisAu = 0.0;
+        double eccentricity = 0.0;
+        double perihelionArgumentDegrees = 0.0;
+    };
+
+    explicit StarSystemTopView(QWidget *parent = nullptr);
+
+    void setPlanets(const QVector<PlanetOrbit> &planets);
+    void setSelectedIndex(int index);
+    int selectedIndex() const;
+
+signals:
+    void planetSelected(int index);
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+
+private:
+    QVector<QPointF> planetScreenPositions(const QSize &size) const;
+    double maxOrbitDistanceAu() const;
+
+    QVector<PlanetOrbit> planets_;
+    int selectedIndex_ = -1;
+};
