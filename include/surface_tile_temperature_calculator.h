c06bcea6c2e24175644b9188de639137323853b7
TopoLM21 <topoln21@gmail.com>
2026-01-16 11:30:06 +0300

    Use continuous elapsed days for substellar phase

--- include/surface_tile_temperature_calculator.h
+++ include/surface_tile_temperature_calculator.h
@@ -20,6 +20,8 @@ struct SurfaceTileTemperatureSettings {
     double segmentSolarConstant = 0.0;
     double dayLengthDays = 0.0;
     double yearLengthDays = 0.0;
+    // Время с начала симуляции в солнечных сутках (не дискретный индекс шага).
+    double elapsedDays = 0.0;
     RotationMode rotationMode = RotationMode::Normal;
     double declinationDegrees = 0.0;
     double orbitalPhaseRadians = 0.0;
